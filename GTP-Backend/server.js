ccbd4ce0d19eecc6d4e63bc0e3ce3d563a9695ec
Alfonso Garcia <ajgarcia321@gmail.com>
2020-03-13 15:41:50 -0500

    Models & Controllers setup + Server.js Setup

--- GTP-Backend/server.js
+++ GTP-Backend/server.js
@@ -1,2 +1,33 @@
-res.send('Hello World')
-console.log('Hello World');
+const express = require('express')
+const app = express()
+const PORT = 3003
+const gtpController = require('./controllers/gtp.js')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+// Middleware //
+app.use(express.json())
+
+const whitelist = ['http://localhost:3000', 'https://fathomless-sierra-68956.herokuapp.com']
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+// app.use(cors(corsOptions))
+
+mongoose.connection.on('error', error => { console.log(error.message + 'Remember to run mongo or something')})
+mongoose.connection.on('disconnected', ()=> console.log('Mongoose Disconnected'))
+mongoose.connect('mongodb://localhost:27017/gtps', {useUnifiedTopology: true, useNewUrlParser: true})
+mongoose.connection.once('open', () => {console.log('Mongoose Connected')})
+
+/* Put our routes below */
+app.use('/gtps', gtpController)
+
+app.listen(PORT, ()=> {
+    console.log('Everything is going according to plan at port: ', PORT)
+})
